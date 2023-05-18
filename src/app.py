d17714293cd2160e0b96ba06e31b633f122c9f96
ORLANDO CHOQUE AYMA <56332894+ochoque@users.noreply.github.com>
2023-05-18 01:17:24 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -111,4 +111,4 @@ def pagina_no_encontrada(error):
 if __name__ == '__main__':
     app.config.from_object(config['development'])
     app.register_error_handler(404, pagina_no_encontrada)
-    app.run()
+    app.run(host='0.0.0.0')
