181316aabfab38507e9c34bb6de51bec21976a8e
Orlandou <orlando_choque@hotmail.com>
2023-05-18 23:38:39 -0400

    respaldo v.3

--- src/connecion.py
+++ src/connecion.py
@@ -1,12 +1,12 @@
 import psycopg2
 def conneccion():
-    #PG_HOST = 'dpg-chiqk45269v2e2bblbgg-a'
-    #PG_USER = 'db_api_flask_user'
-    #PG_PASSWORD = 'XBfDf1jS5To8OYMNT3zE1ImVglTdZOKk'
-    #PG_DB = 'db_api_flask'
-    PG_HOST = 'localhost'
-    PG_USER = 'postgres'
-    PG_PASSWORD = 'postgres'
+    PG_HOST = 'dpg-chiqk45269v2e2bblbgg-a'
+    PG_USER = 'db_api_flask_user'
+    PG_PASSWORD = 'XBfDf1jS5To8OYMNT3zE1ImVglTdZOKk'
     PG_DB = 'db_api_flask'
+    #PG_HOST = 'localhost'
+    #PG_USER = 'postgres'
+    #PG_PASSWORD = 'postgres'
+    #PG_DB = 'db_api_flask'
     con = psycopg2.connect(database=PG_DB, host=PG_HOST, user=PG_USER, password=PG_PASSWORD, port=5432)
     return con
